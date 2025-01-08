f46594af42152697fc5b3bfd3f0f558b2c8aceb3
aszymeczko <agata.szymeczko21@gmail.com>
2025-01-08 20:32:24 +0100

    Add search to redux

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,23 @@
 import styles from "./SearchForm.module.scss";
 import TextInput from "../TextInput/TextInput";
 import Button from "../Button/Button";
+import { useDispatch, useSelector } from "react-redux";
 
 const SearchForm = () => {
+  const searchString = useSelector((state) => state.searchString);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    dispatch({ type: "UPDATE_SEARCHSTRING", payload: e.target.value });
+  };
+
   return (
     <form className={styles.searchForm}>
-      <TextInput placeholder="Search..." />
+      <TextInput
+        placeholder="Search..."
+        value={searchString}
+        onChange={(e) => handleChange(e)}
+      />
       <Button>
         <span className="fa fa-search" />
       </Button>
