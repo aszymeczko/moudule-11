0e133d3511e591b500ce9c2530b9262b3d1be45d
aszymeczko <agata.szymeczko21@gmail.com>
2025-01-09 20:43:18 +0100

    Add deleteCard action

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import styles from "./Card.module.scss";
 import { useDispatch } from "react-redux";
 import clsx from "clsx";
-import { toggleCardFavorite } from "../../redux/cardsRedux";
+import { toggleCardFavorite, deleteCard } from "../../redux/cardsRedux";
 
 const Card = (props) => {
   const dispatch = useDispatch();
@@ -9,12 +9,21 @@ const Card = (props) => {
     dispatch(toggleCardFavorite(props.id));
   };
 
+  const handleDelete = () => {
+    dispatch(deleteCard(props.id));
+  };
+
   return (
     <li className={clsx(styles.card, props.isFavorite ? styles.active : "")}>
       {props.title}
-      <button onClick={() => handleClick()}>
-        <i className="fa fa-star-o"></i>
-      </button>
+      <div className={styles.buttonsWrapper}>
+        <button className={styles.addToFavorite} onClick={() => handleClick()}>
+          <i className="fa fa-star-o"></i>
+        </button>
+        <button className={styles.remove} onClick={() => handleDelete()}>
+          <i className="fa fa-trash"></i>
+        </button>
+      </div>
     </li>
   );
 };
--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -8,7 +8,10 @@ export const getFavoritesCards = ({ cards }) =>
 const createActionName = (actionName) => `app/cards/${actionName}`;
 const ADD_CARD = createActionName("ADD_CARD");
 const TOGGLE_CARD_FAVORITE = createActionName("TOGGLE_CARD_FAVORITE");
+const DELETE_CARD = createActionName("DELETE_CARD");
 export const addCard = (payload) => ({ type: ADD_CARD, payload });
+
+export const deleteCard = (payload) => ({ type: DELETE_CARD, payload });
 export const toggleCardFavorite = (payload) => ({
   type: TOGGLE_CARD_FAVORITE,
   payload,
@@ -23,6 +26,9 @@ const cardsRedux = (statePart = [], action) => {
           ? { ...card, isFavorite: !card.isFavorite }
           : card,
       );
+    case DELETE_CARD:
+      return statePart.filter((card) => card.id !== action.payload);
+
     default:
       return statePart;
   }
